8d3966a3ae5569db9fb89a551d5840a35510e9f3
Django <sara@kapost.com>
2017-02-05 15:20:44 -0700

    resume updates

--- js/app.js
+++ js/app.js
@@ -49,34 +49,6 @@ $(document).foundation();
       image: 'images/kp-flatirons.png',
       class: 'kp-flatirons',
       link: '#',
-    },
-    {
-      name: 'Kapost, App Icons',
-      description: 'Design and Illustration; Tools: Adobe Illustrator',
-      image: 'images/kp-icons-more.png',
-      class: 'kp-icons-more',
-      link: '#',
-    },
-    {
-      name: 'Bb, Gradebook',
-      description: 'UI/UX Design, Wireframes, Application Flows, User Testing, Prototypes, Front-End Development',
-      image: 'images/my-gradebooks.png',
-      class: 'my-gradebooks',
-      link: '#',
-    },
-    {
-      name: 'Blackboard Learner',
-      description: 'Digital Illustration, Layout, Design; Tools: Adobe Illustrator',
-      image: 'images/bb-learner.png',
-      class: 'bb-learner',
-      link: '#',
-    },
-    {
-      name: 'Blackboard Ultra',
-      description: 'Collaborated on UI Design, Wireframes, Hi-Fidelity Prototypes, Front-End Development',
-      image: 'images/bb-ultra.png',
-      class: 'bb-ultra',
-      link: '#',
     }
   ];
 })();
